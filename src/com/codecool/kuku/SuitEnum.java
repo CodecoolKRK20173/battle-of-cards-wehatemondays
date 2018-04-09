0d8456fd84e7e47231b5ecac4822c2f1350381ce
Monika Górecka <monika.gorecka.agh@gmail.com>
2018-04-09 22:21:45 +0200

    Add SuitEnum file

---
+++ src/com/codecool/kuku/SuitEnum.java
@@ -0,0 +1,17 @@
+public enum SuitEnum {
+    HEARTS(1),
+    DIAMONDS(2),
+    SPADES(3),
+    CLUBS(4);
+
+    private final int suit;
+
+    SuitEnum(int suit) {
+        this.suit = suit;
+    }
+
+    public int getSuit() {
+        return this.suit;
+    }
+
+}
