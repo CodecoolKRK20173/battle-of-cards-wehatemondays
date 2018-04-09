771a75fffac13da2767c8f27e7e2d33075a33699
wluka85 <wluka85@gmail.com>
2018-04-10 00:14:50 +0200

    Implement main method in Main class

---
+++ src/com/codecool/kuku/Main.java
@@ -0,0 +1,24 @@
+package com.codecool.kuku;
+
+import com.codecool.kuku.display.*;
+import java.awt.*;
+import javax.swing.*;
+
+
+public class Main{
+    public static void main(String[] args){
+
+        EventQueue.invokeLater(new Runnable()
+            {
+                public void run() {
+                    KukuFrame kukuFrame = new KukuFrame();
+                    kukuFrame.setTitle("Kuku");
+                    kukuFrame.setLocationRelativeTo(null);
+                    kukuFrame.setResizable(false);
+                    kukuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    kukuFrame.setVisible(true);
+                }
+
+            });
+    }
+}
