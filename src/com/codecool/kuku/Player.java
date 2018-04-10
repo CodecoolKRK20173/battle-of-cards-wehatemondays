b43c339533d9bd6976311350ee08626a1afbf4bb
Riczard <riczardkiller@gmail.com>
2018-04-10 09:23:00 +0200

    Add abstract class with player logic

---
+++ src/com/codecool/kuku/Player.java
@@ -0,0 +1,28 @@
+package com.codecool.kuku;
+
+import java.util.ArrayList;
+
+/**
+ * This is a abstract class representing players.
+ */
+public abstract class Player{
+    /**
+     * check cards for possible kuku.
+     * @return true if kuku
+     */
+    public abstract boolean checkKuku();
+
+    /**
+     * Calculates best move.
+     * @return card to give
+     */
+    public abstract card handleNextMove();
+
+    /**
+     * Add  cards to list.
+     * @return list of used cards
+     */
+    public abstract ArrayList handleCardsOnHand();
+
+    public abstract HashMap possiblessOpponentsKuku();
+}
