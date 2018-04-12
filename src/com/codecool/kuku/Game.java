ae21024e5ce8ce9c1eac4f10e4c770bd44a5a187
Riczard <riczardkiller@gmail.com>
2018-04-12 21:44:40 +0200

    Refactor game class

--- src/com/codecool/kuku/Game.java
+++ src/com/codecool/kuku/Game.java
@@ -1,6 +1,5 @@
 package com.codecool.kuku;
 
-
 import java.util.Iterator;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,47 +25,33 @@ public class Game {
     }
 
     public void handleRound(Card cardHuman) {
-        StringBuilder gameLog = new StringBuilder();
-        removeFromPlayersList(); //if player has kuku TODO
+        Card cardToPass;
+
+        removeFromPlayersList();
 
         for (int i = 0; i < players.size(); i++) {
-
             Player currentPlayer = players.get(i);
-            gameLog.append(currentPlayer.getPlayerName() + "'s turn:\n'");
-
             if (currentPlayer.checkKuku()) playersWithKuku.add(currentPlayer);
-            // /// adding to kuku list
-            //gameLog.append(currentPlayer.getPlayerName() + " have reported Kuku!\n");
-            // playersWithKuku.add(currentPlayer);
-            // /// checking kuku mechanics
-            Card cardToPass; //////// ->>>> what card?
+
             if (currentPlayer instanceof Human) {
                 cardToPass = cardHuman;
-                gameLog.append("You have passed " + cardToPass.toString() + " to ");
-
             }
             else {
                 cardToPass = currentPlayer.handleNextMove();
-                gameLog.append(players.get(i).getPlayerName() + " passed card to ");
             }
-
             if (i + 1 < players.size()) {
                 players.get(i+1).getPile().addCard(cardToPass);
                 players.get(i).getPile().removeFromPile(cardToPass);
-                gameLog.append(players.get(i+1).getPlayerName() + ". \n\n");
             }
             else {
                 allHumanCards.add(cardToPass);
                 players.get(0).getPile().addCard(cardToPass);
                 players.get(i).getPile().removeFromPile(cardToPass);
-                gameLog.append(players.get(0).getPlayerName() + ". \n\n");
             }
         }
-        String turnLog = gameLog.toString();
     }
 
      public boolean isRoundDone() {
-         System.out.println(players.size());
         if (players.size() == 1) {
             return true;
         }
@@ -83,14 +68,12 @@ public class Game {
     }
 
     private void dealCards() {
-        // Collections.shuffle(players);
         Iterator<Card> deckIterator = deck.iterator();
         int NUMBEROFCARDSKUKU = 3;
         for (int i = 0; i < NUMBEROFCARDSKUKU; i++) {
             for (Player x : players) {
                 Card card = deckIterator.next();
                 x.getPile().addCard(card);
-                System.out.println("Adding one card to one player");
             }
         }
         players.get(0).getPile().addCard(deckIterator.next());
@@ -98,19 +81,6 @@ public class Game {
             stock.addCard(card);
         });
     }
-
-    public void setHumanKuku() {
-    // public boolean canHumanKuku(Human human){
-    //     return human.checkKuku();
-    // }
-
-    // More likely like thizzz, but I'm not sure if catch this right.
-    // if not I'll sort that one out tomo
-
-        //user has kuku (from GUI)
-        // check it!!
-
-    };
 
     public boolean checkIfKuku() {
         return players.get(0).checkKuku();
@@ -132,31 +102,25 @@ public class Game {
         return playersWithKuku;
     }
 
-    // public ifPlayerGuess() {
-    //     //if player guess right suit or rank
-    // }
-
     public void removeFromPlayersList() {
 
         if (!playersWithKuku.isEmpty()) {
             for (int i = 0; i < playersWithKuku.size(); i++) {
                 Player playerWithKuku = playersWithKuku.get(i);
-
-                for (int x = 0; x < players.size(); x++) {
-                    if (players.get(x).equals(playerWithKuku)) {
-                        players.remove(x);
-                    }
-                }
+                removePlayerwithKuku(playerWithKuku);
             }
         }
     }
-    public boolean ifPlayerGuess() {
-        //if player guess right suit or rank
-        return true;
+    
+    public void removePlayerwithKuku(Player playerWithKuku){
+        for (int x = 0; x < players.size(); x++) {
+            if (players.get(x).equals(playerWithKuku)) {
+                players.remove(x);
+            }
+        }
     }
 
     public List<Player> getPlayers() {
         return players;
     }
-
 }
